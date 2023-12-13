2ab0efa2562495788ed2a254a60e960a957b37e4
Jack Yang <jack.yang_irisgestao@oriongrupo.onmicrosoft.com>
2023-12-13 14:47:51 -0300

    First changes

---
+++ src/Pages/About/index.js
@@ -0,0 +1,13 @@
+import {
+    FullPage
+} from './styles.ts'
+
+export default function AboutPage() {
+
+    return (
+        <FullPage>
+            Teste
+
+        </FullPage>
+    )
+}
---
+++ src/Pages/Home/index.js
@@ -0,0 +1,13 @@
+import {
+    FullPage
+} from './styles.ts'
+
+export default function HomePage() {
+
+    return (
+        <FullPage>
+            Bla
+
+        </FullPage>
+    )
+}
