16859d8005daeb861836829f0b4e8025862b086f
Jack Yang <jack.yang_irisgestao@oriongrupo.onmicrosoft.com>
2023-12-14 15:01:49 -0300

    Fetch google excel data

--- src/Pages/About/styles.ts
+++ src/Pages/About/styles.ts
@@ -3,5 +3,4 @@ import styled from 'styled-components';
 export const FullPage = styled.div`
     width: 100%;
     height: 100vh;
-    background-color: green;
 `;
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,9 +1,35 @@
+import { async } from 'q'
 import {
     FullPage
 } from './styles.ts'
+import { useEffect } from 'react';
+import axios from 'axios';
+import * as xlsx from 'xlsx';
 
 export default function HomePage() {
 
+    const getData = async () => {
+        const response = await axios({
+            method: 'GET',
+            url: `https://docs.google.com/spreadsheets/d/1oF3vMoHrwIUzWCQ_neEemfAqR279NpHXn7HaSxrXc_U/edit#gid=0`,
+            responseType: 'arraybuffer',
+        });
+
+        const workbook = xlsx.read(response.data, { type: 'array' });
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+
+        // Process each cell in the sheet
+        const cells = xlsx.utils.sheet_to_json(sheet, { header: 1, raw: true });
+
+        // Now 'cells' is an array representing each row in the spreadsheet
+
+        console.log('Spreadsheet Data:', cells);
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
     return (
         <FullPage>
             Bla
